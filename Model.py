3d33cf9cdbe371cfccdedeb84b4ae31549e22951
hsoffar <31486587+hsoffar@users.noreply.github.com>
2018-05-23 23:26:17 +0200

    update
    upload

---
+++ Model.py
@@ -0,0 +1,189 @@
+import tensorflow as tf
+from keras.layers import Dense, Flatten, Lambda, Activation, MaxPooling2D
+from keras.layers.convolutional import Convolution2D
+from keras.models import Sequential
+from keras.optimizers import Adam
+import errno
+import json
+import os
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.misc
+from scipy.ndimage import rotate
+from scipy.stats import bernoulli
+
+
+tf.python.control_flow_ops = tf
+# Some useful constants
+DRIVING_LOG_FILE = './../../data/data/driving_log.csv'
+IMG_PATH = './../../data/data/'
+STEERING_COEFFICIENT = 0.2
+
+num_epochs = 7
+num_samples_per_epoch = 20000
+num_validation_samples = 3000
+rate = 0.0001
+def crop(image, top_percent, bottom_percent):
+    top = int(np.ceil(image.shape[0] * top_percent))
+    bottom = image.shape[0] - int(np.ceil(image.shape[0] * bottom_percent))
+    return image[top:bottom, :]
+
+def resize(image, new_dim):
+    return scipy.misc.imresize(image, new_dim)
+
+def random_flip(image, steering_angle, flipping_prob=0.5):
+    head = bernoulli.rvs(flipping_prob)
+    if head:
+        return np.fliplr(image), -1 * steering_angle
+    else:
+        return image, steering_angle
+
+def random_gamma(image):
+    # apply gamma correction using the lookup table
+    return cv2.LUT(image, table)
+
+def random_shear(image, steering_angle, shear_range=200):
+    rows, cols, ch = image.shape
+    dx = np.random.randint(-shear_range, shear_range + 1)
+    random_point = [cols / 2 + dx, rows / 2]
+    pts1 = np.float32([[0, rows], [cols, rows], [cols / 2, rows / 2]])
+    pts2 = np.float32([[0, rows], [cols, rows], random_point])
+    dsteering = dx / (rows / 2) * 360 / (2 * np.pi * 25.0) / 6.0
+    M = cv2.getAffineTransform(pts1, pts2)
+    image = cv2.warpAffine(image, M, (cols, rows), borderMode=1)
+    steering_angle += dsteering
+
+    return image, steering_angle
+
+def random_rotation(image, steering_angle, rotation_amount=15):
+    angle = np.random.uniform(-rotation_amount, rotation_amount + 1)
+    rad = (np.pi / 180.0) * angle
+    return rotate(image, angle, reshape=False), steering_angle + (-1) * rad
+def min_max(data, a=-0.5, b=0.5):
+    data_max = np.max(data)
+    data_min = np.min(data)
+    return a + (b - a) * ((data - data_min) / (data_max - data_min))
+
+def generate_new_image(image, steering_angle, top_crop_percent=0.35, bottom_crop_percent=0.1,
+                       resize_dim=(64, 64), do_shear_prob=0.9):
+    head = bernoulli.rvs(do_shear_prob)
+    if head == 1:
+        image, steering_angle = random_shear(image, steering_angle)
+
+    image = crop(image, top_crop_percent, bottom_crop_percent)
+
+    image, steering_angle = random_flip(image, steering_angle)
+
+    image = random_gamma(image)
+
+    image = resize(image, resize_dim)
+
+    return image, steering_angle
+
+
+def get_next_image_files(batch_size=64):
+    data = pd.read_csv(DRIVING_LOG_FILE)
+    num_of_img = len(data)
+    rnd_indices = np.random.randint(0, num_of_img, batch_size)
+
+    image_files_and_angles = []
+    for index in rnd_indices:
+        rnd_image = np.random.randint(0, 3)
+        if rnd_image == 0:
+            img = data.iloc[index]['left'].strip()
+            angle = data.iloc[index]['steering'] + STEERING_COEFFICIENT
+            image_files_and_angles.append((img, angle))
+
+        elif rnd_image == 1:
+            img = data.iloc[index]['center'].strip()
+            angle = data.iloc[index]['steering']
+            image_files_and_angles.append((img, angle))
+        else:
+            img = data.iloc[index]['right'].strip()
+            angle = data.iloc[index]['steering'] - STEERING_COEFFICIENT
+            image_files_and_angles.append((img, angle))
+
+    return image_files_and_angles
+
+
+def generate_next_batch(batch_size=64):
+    while True:
+        X_batch = []
+        y_batch = []
+        images = get_next_image_files(batch_size)
+        for img_file, angle in images:
+            raw_image = plt.imread(IMG_PATH + img_file)
+            raw_angle = angle
+            new_image, new_angle = generate_new_image(raw_image, raw_angle)
+            X_batch.append(new_image)
+            y_batch.append(new_angle)
+
+        assert len(X_batch) == batch_size, 'len(X_batch) == batch_size should be True'
+
+        yield np.array(X_batch), np.array(y_batch)
+
+
+
+
+model = Sequential()
+
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(64, 64, 3)))
+
+# starts with five convolutional and maxpooling layers
+model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Flatten())
+
+# Next, five fully connected layers
+model.add(Dense(1164))
+model.add(Activation('relu'))
+
+model.add(Dense(100))
+model.add(Activation('relu'))
+
+model.add(Dense(50))
+model.add(Activation('relu'))
+
+model.add(Dense(10))
+model.add(Activation('relu'))
+
+model.add(Dense(1))
+
+model.summary()
+
+model.compile(optimizer=Adam(rate), loss="mse", )
+
+# create two generators for training and validation
+train_gen = helper.generate_next_batch()
+validation_gen = helper.generate_next_batch()
+
+history = model.fit_generator(train_gen,
+                              samples_per_epoch=num_samples_per_epoch,
+                              nb_epoch=num_epochs,
+                              validation_data=validation_gen,
+                              nb_val_samples=num_validation_samples,
+                              verbose=1)
+
+# finally save our model and weights
+model.save ('model.h5')
+exit()
